e78bb6d6118176730aed705200477e7f7276989e
zakhiz <sebascursoshtml@gmail.com>
2022-07-27 22:03:28 -0300

    simulador interactivo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let iva = true;
+/*let iva = true;
 
  while(iva){
      let precioIva = parseFloat(prompt("Ingresar monto a calcular el iva"));
@@ -11,7 +11,36 @@ let iva = true;
     }else{
         alert("Caracter no reconocido")
     }
- }
+ }*/
+const compraDolar = 326.00;
+const ventaDolar =321.00;
+const comprarDolar = (num) => (num / compraDolar);
+const venderDolar = (num) => (num * ventaDolar);
+alert("Hola Bienvenido/a al convertidor de peso a dolar");
+alert("Escribe compra o venta segun la operacion que desees hacer");
+let continuar;
+do{
+let numero , cambio;
+do{
+    numero = parseFloat(prompt("Ingrese monto a convertir"));
+    cambio = prompt("Ingrese que tipo de operacion desea hacer (compra o venta)").toLowerCase();
+    if(isNaN(numero)){
+        alert("numero invalido")
+    }
 
-
+}while(isNaN(numero) || (numero === 0 && cambio != ""));
+switch(cambio){
+    case "compra":
+        console.log(comprarDolar(numero));
+        break;
+    case "venta":
+        console.log(venderDolar(numero));
+        break;
+    default:
+        alert("Operacion invalida");     
+}
+do{
+    continuar= prompt("¿Desea hacer otra conversion?").toLowerCase();
+}while(continuar != "si" && continuar != "no");
+}while(continuar!="no");
 
